b4b0c98f58b670ca534fe8026a96e323f5f78f75
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-12 01:34:03 +0100

    implement root layout and constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '@/assets/styles/globals.css';
+import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Store",
-  description:
-    "A modern store like a fairy tale built with Next.js, ShadCN, and Prisma.",
+  title: { template: `%s | My App`, default: APP_NAME },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const HomePage = () => {
-  return <>Latest Products</>;
-};
-
-export default HomePage;
