4ee391308200b6bbf57044486f001a47a595d704
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-13 12:25:09 +0100

    add product details page

--- lib/actions/product.actions.tsx
+++ lib/actions/product.actions.tsx
@@ -12,3 +12,13 @@ export async function getLatestProducts() {
 
   return convertToPlainObject(data);
 }
+
+
+// Get single product by slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
+
+
