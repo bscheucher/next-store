d9b21f48416b2851b3b83fd61cc3666210ab0f90
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-14 04:29:05 +0100

    add user button, logout functionality

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/sheet";
 import Link from "next/link";
 import ModeToggle from "./mode-toggle";
+import UserButton from "./user-button";
 
 const Menu = () => {
   return (
@@ -22,12 +23,7 @@ const Menu = () => {
               Cart
             </Link>
           </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
+          <UserButton/ >
         </nav>
         <nav className="md:hidden">
           <Sheet>
@@ -44,10 +40,7 @@ const Menu = () => {
                 </Link>
               </Button>
               <Button asChild>
-                <Link href="/sign-in">
-                  <UserIcon />
-                  Sign In
-                </Link>
+                <UserButton />
               </Button>
               <SheetDescription></SheetDescription>
             </SheetContent>
