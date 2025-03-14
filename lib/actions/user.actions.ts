3607cc7121f518f8a4aed00a67e888365a96860f
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-14 23:14:43 +0100

    add format error to sign up

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,7 +4,7 @@ import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { signInFormSchema, signUpFormSchema } from "../validators";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
-
+import { formatError } from '../utils';
 
 
 export async function signInWithCredentials(
@@ -70,7 +70,7 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 
     return {
       success: false,
-      message: "Something went wrong",
+      message: formatError(error),
     };
   }
 }
