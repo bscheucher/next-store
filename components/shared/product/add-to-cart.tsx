50d166f49ce5f38c98327da02e558a1b4a60dacc
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-16 14:50:53 +0100

    add add-item-to-cart component, card action

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { toast, Toaster } from "sonner";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+import { CartItem } from "@/types";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+
+  const handleAddToCart = async () => {
+    // Execute the addItemToCart action
+    const res = await addItemToCart(item);
+
+    // Display appropriate toast message based on the result
+    if (!res.success) {
+      toast.error(res.message, {
+        style: {
+          color: "red", // Change the text color to red
+          border: "1px solid red", // Add a red border
+        },
+      });
+      return;
+    }
+
+    toast.success(`${item.name} added to the cart`, {
+      action: {
+        label: "Go to cart",
+        onClick: () => router.push("/cart"),
+      },
+    });
+  };
+
+  return (
+    <>
+      <Button className="w-full" type="button" onClick={handleAddToCart}>
+        Add To Cart
+      </Button>
+      <Toaster />
+    </>
+  );
+};
+
+export default AddToCart;
