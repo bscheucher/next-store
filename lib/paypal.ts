b39518f1f011ff464dc11a18e55ce221f783b182
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-21 20:03:52 +0100

    create tests for order payment

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,8 +1,45 @@
 const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
 
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "EUR",
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
 
+    return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponse(response);
+  },
+};
 
 // Generate an access token for the PayPal API
 export async function generateAccessToken() {
@@ -24,12 +61,11 @@ export async function generateAccessToken() {
   return jsonData.access_token;
 }
 
-
 // Reusable function for response handling
 async function handleResponse(response: any) {
-    if (response.status === 200 || response.status === 201) {
-      return response.json();
-    }
-    const errorMessage = await response.text();
-    throw new Error(errorMessage);
+  if (response.status === 200 || response.status === 201) {
+    return response.json();
   }
+  const errorMessage = await response.text();
+  throw new Error(errorMessage);
+}
