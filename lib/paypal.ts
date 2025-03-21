2ac40cb22ca29ea5fff51bf4f49d177f51c8f03c
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-21 11:37:22 +0100

    generate paypal token

---
+++ lib/paypal.ts
@@ -0,0 +1,33 @@
+const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
+
+export const paypal = {};
+
+// Generate an access token for the PayPal API
+async function generateAccessToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    "base64"
+  );
+
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: "POST",
+    body: "grant_type=client_credentials",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+
+// Reusable function for response handling
+async function handleResponse(response: any) {
+    if (response.status === 200 || response.status === 201) {
+      return response.json();
+    }
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
