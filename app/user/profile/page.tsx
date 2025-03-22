4935b6482c9f601494efae366391536dc2775a73
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-22 23:21:59 +0100

    add update user form

---
+++ app/user/profile/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import ProfileForm from "./profile-form";
+
+export const metadata: Metadata = {
+  title: "Customer Profile",
+};
+
+export default async function ProfilePage() {
+  const session = await auth();
+  if (!session || !session.user) {
+    throw new Error("User is not authenticated");
+  }
+
+  return (
+    <SessionProvider session={session}>
+      <div className="max-w-md  mx-auto space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
+  );
+}
